a90d5ecf80ff185cae04bc0360e51d25cf048292
Yves Jaques <ymoraisjaques@gmail.com>
2025-08-10 15:28:41 -0300

    Sending Login Requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,13 +4,14 @@ import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 import { environment } from "src/environments/environment";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean
 }
 
 @Injectable({ providedIn: 'root' })
@@ -46,9 +47,18 @@ export class AuthService {
             return throwError(errorMessage);
         }))
     }
-    login() {
-        this.loggedIn = true;
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        )
     }
+
     logout() {
         this.loggedIn = false;
     }
